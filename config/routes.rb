2510d4152ffe24d5badc8c58d67de41508bd0d85
Federico <federico.capaldo@hotmail.com>
2023-05-21 19:25:14 +0100

    shorthand for home route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/", to: "pages#home"
+  root to: "pages#home"
 
   get("about", { to: "pages#about" })
 

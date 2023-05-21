ea73818a8f6c8c54cfa2df9c0aefe203637789c6
Federico <federico.capaldo@hotmail.com>
2023-05-21 20:19:00 +0100

    refactored to put contacts into contact page, and made example with paramters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,19 @@ class PagesController < ApplicationController
   end
 
   def about
-    @members = ["thanh", "dimitri", "germain", "damien", "julien"]
   end
 
   def contact
+    @members = ["thanh 📞: 123", "dimitri 📞: 345", "germain 📞: 456", "damien 📞: 567", "julien 📞: 678"]
+
+    search = params[:member]
+
+    if search
+      @members = @members.select do |m|
+        m.start_with? search
+      end
+    end
+
   end
 
 end

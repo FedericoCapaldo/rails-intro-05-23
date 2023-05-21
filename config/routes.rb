2db34660b84cb5f4bf9d23d48d7a326df5b88561
Federico <federico.capaldo@hotmail.com>
2023-05-21 19:24:36 +0100

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "/", to: "pages#home"
+
   get("about", { to: "pages#about" })
 
   get "contact", to: "pages#contact"
